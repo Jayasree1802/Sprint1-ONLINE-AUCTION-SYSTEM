0ee1e93ff95bacb44cbaaf77d990a95c42f3775c
Jayasree1802 <121030796+Jayasree1802@users.noreply.github.com>
2022-12-26 00:47:32 +0530

    Update admin.c

--- CUT/CODE/SRC/admin.c
+++ CUT/CODE/SRC/admin.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 #include<string.h>
-#include"prototype.h"
-#include"buyer_prototype.h"
-#include"admin_prototype.h"
+#include"header.h"
+
 
 void admin(seller_Details *seller_root , buyer_Details *buyer_root)
 {
