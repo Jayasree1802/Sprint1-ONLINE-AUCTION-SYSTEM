f3187df9c8cd5ca252e2ee1396cdde770dc1887b
Jayasree1802 <121030796+Jayasree1802@users.noreply.github.com>
2022-12-29 08:56:10 +0530

    Add files via upload

--- CUT/CODE/SRC/validations.c
+++ CUT/CODE/SRC/validations.c
@@ -20,32 +20,28 @@
 int intiger_validation(char *str)
 {
 	long int num ;
-
 	int len =0 , count = 0;
-	
 	num = atoi(str);
-	
 	len = strlen(str);
-	
 	if(num <= 0)
 	{
-		return 1;
+		return 0;
 	}
 	else
 	{	
 		do {
-		
 			num /= 10;
 			++count;
 			
-		  }while(num != 0);
-		  
+		  }while(num != 0); 
 		  if(len == count)
-		  	return 0;
+		  	return 1;
 		  else 
-		  	return 1;	
+		  	return 0;	
         }
 }
+
+
 /*******************************************************************************************
 **  FUNCTION NAME   : alpha_validator
 **  DESCRIPTION     : It is the function for validating the alphabets
@@ -63,7 +59,7 @@ int alpha_validator(char *s)
                         return 0;
         }
         //validation for length
-        if(strlen(s)>15 || strlen(s) < 5)
+        if(strlen(s)>15 || strlen(s) < 3)
         {
                 printf("Enter name within the length of 5 to 15\n");
                 return 0;
@@ -77,19 +73,12 @@ int alpha_validator(char *s)
 **  PARAMETERS      : parameter of character pointer
 **  RETURN          :  No Return 
 *******************************************************************************************/
+
 int phone_validation(char *str)
 {
-	
 	long int num ;
-
 	int len =0 , count = 0;
-	
 	len = strlen(str);
-	
-	
-	
-	
-	
 	if(num <= 0 || len < 10 || len > 10)
 	{
 		return 0;
@@ -111,12 +100,14 @@ int phone_validation(char *str)
         }	
 }
 
+
 /*******************************************************************************************
 **  FUNCTION NAME   : password_validation
 **  DESCRIPTION     : It is the function to validate the password
 **  PARAMETERS      : character string is parameter
 **  RETURN          :  No Return 
 *******************************************************************************************/
+
 int password_validation(char *str)
 {
 	char *ptr = str ;
@@ -152,4 +143,144 @@ int password_validation(char *str)
 	return 1;
 	else 
 	return 0;
+}
+
+int int_ans_choice(int n , int m)
+{
+	int ch;
+	char temp[10];
+	
+		while(1)
+		{
+			scanf("%s",temp);
+			if(intiger_validation(temp))
+			{
+				ch = atoi(temp);
+				if(ch >= n && ch <= m)
+				{
+					ch = atoi(temp);
+					break;
+				}
+				else 
+					printf("enter number with range of %d to %d \n",n,m);
+				
+			}
+			else
+			{
+				printf(" int enter valid input\n");
+				return -1;
+			}
+		}
+	return ch;
+}
+int int_id_validation()
+{
+	char int_reading[10];
+	
+	while(1)
+	{
+		scanf("%s",int_reading);
+		int_reading[strlen(int_reading)] = '\0';
+		
+		if(intiger_validation(int_reading))
+		{
+			return atoi(int_reading);
+			break;
+		}	
+		else
+		printf("Product-Id is in intiger farmat only\n");		
+	}
+}
+
+/*******************************************************************************************
+**  FUNCTION NAME   : Emai_validation
+**  DESCRIPTION     : It is the function validate the email for buyer and seller
+**  PARAMETERS      : passing parameter as char type of string
+**  RETURN          :  No Return 
+*******************************************************************************************/
+
+int Emai_validation(char *str)
+{
+	char *ptr = str ;
+	
+	int c_count = 0 , s_count = 0 , i_count = 0 ,e_count =0 ; 
+	
+	while(*ptr !='\0')
+	{
+		if(*ptr >= 'A' && *ptr <= 'Z')	
+		{
+			c_count = 1;	
+			//printf("Capital\n");
+		}
+		else if(*ptr >= 'a' && *ptr <= 'z')
+		{
+			s_count = 1;
+			//printf("small\n");
+		}
+		else if(*ptr >= 48 && *ptr <= 58 )
+		{
+			
+			//printf("num\n");
+		}
+		else if(*ptr == '@')
+		{
+			e_count = 1;
+		}
+		else if(*ptr == '.')
+		{
+			i_count = 1;
+		}
+		
+		
+		ptr++;
+	}
+	
+	if(e_count == 1 && i_count == 1 )
+	return 1;
+	else 
+	return 0;	
+}
+/*******************************************************************************************
+**  FUNCTION NAME   : validate_pan
+**  DESCRIPTION     : It is the function validate the pan for buyer and seller
+**  PARAMETERS      : character of pan no
+**  RETURN          :  No Return 
+*******************************************************************************************/
+
+int validate_Pan(char *Pan_No)
+{
+
+       int i = 0 , c_flag = 0 , n_flag = 0 , s_flag =0 ;
+
+	//printf("pan number len %d\n",strlen(Pan_No));
+        
+		while(Pan_No[i]!='\0')
+		{
+			if( (Pan_No[i] >='A'&& Pan_No[i] <= 'Z') )
+			{
+
+			       c_flag = 1;
+			}
+
+			if(  (Pan_No[i]>48 && Pan_No[i]<57) )
+			{
+
+				n_flag = 1;
+			}
+		       if( (Pan_No[i] >='A' && Pan_No[i] <='Z' ) )
+			{
+
+			       s_flag = 1 ;
+			}
+			i++;
+               }
+       
+
+        //printf("\nvalid Pan No");
+
+	if(c_flag == 1 && n_flag == 1 && s_flag == 1)
+        return 1;
+        else
+        return 0;
+
 }
