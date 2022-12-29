b348e9fdec23255e1eb0411ee99f5ee59431e30a
Jayasree1802 <121030796+Jayasree1802@users.noreply.github.com>
2022-12-29 19:56:33 +0530

    Update admin.h

--- CUT/CODE/HEADER/admin.h
+++ CUT/CODE/HEADER/admin.h
@@ -1,15 +1,28 @@
 //header file
 #include<stdio.h>
 
+//macros for admin menu
+
+#define MANAGE_SELLER 1
+#define MANAGE_BUYER 2
+#define MANAGE_PRODUCTS 3
+#define BLOCK_SELLER 1
+#define BLOCK_BUYER  1
+#define LIST_SELLER  2
+#define LIST_BUYER   2
+#define ADD_PRODUCTS  1
+#define REMOVE_PRODUCTS 2
+#define MODIFY_PRODUCTS 3
+#define EXIT 0
+
 //structure of admin data
+
 typedef struct admin_data
 {
-	
 	char user_name[BUFFER_SIZE];
 	char pswd[BUFFER_SIZE];
-	
-	
 	struct admin_data *next ;
+	
 }admin_data;
 
 //functions for admin
