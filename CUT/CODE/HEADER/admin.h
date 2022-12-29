dedabb001325b11d7122f00363ee9dc1bef656a2
Jayasree1802 <121030796+Jayasree1802@users.noreply.github.com>
2022-12-29 09:58:19 +0530

    Update admin.h

--- CUT/CODE/HEADER/admin.h
+++ CUT/CODE/HEADER/admin.h
@@ -1,5 +1,7 @@
+//header file
 #include<stdio.h>
 
+//structure of admin data
 typedef struct admin_data
 {
 	
@@ -10,14 +12,12 @@ typedef struct admin_data
 	struct admin_data *next ;
 }admin_data;
 
+//functions for admin
 
 void admin(seller_Details *, buyer_Details *);
 int admin_login();
 admin_data read_admin_file(admin_data );
-
 product_details *add_product_to_list(product_details *);
-
-
 void block_sller(seller_Details *);
 void list_sller();
 void block_buyer(buyer_Details *);
