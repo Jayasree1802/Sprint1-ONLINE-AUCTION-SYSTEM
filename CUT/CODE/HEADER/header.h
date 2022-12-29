0e8ac266fda1a19e9397fe730b112b85089b7025
Jayasree1802 <121030796+Jayasree1802@users.noreply.github.com>
2022-12-29 21:44:04 +0530

    Update header.h

--- CUT/CODE/HEADER/header.h
+++ CUT/CODE/HEADER/header.h
@@ -1,6 +1,9 @@
+//header file of seller
 
 #include<stdio.h>
 #define BUFFER_SIZE 1000
+
+//structure for seller
 
 typedef struct seller_Details
 {
@@ -21,6 +24,8 @@ typedef struct seller_Details
         struct seller_Details *next;
 }seller_Details;
 
+//structure for product_details
+
 typedef struct product_details
 {
 	int product_id ;
@@ -33,6 +38,8 @@ typedef struct product_details
 	struct product_details *next;
 }product_details;
 
+//structure for bidding details
+
 typedef struct bidding_details
 {
 	int bidding_data[16];
@@ -40,64 +47,36 @@ typedef struct bidding_details
 	struct bidding_details *next ;
 }bidding_details;
 
-
-
+//functions for seller
 
 seller_Details *  price_check(product_details * ,seller_Details *);
 seller_Details *create_lisr_of_seller(seller_Details *,seller_Details);
 seller_Details *file_to_list(seller_Details *);
 seller_Details question_extraction_from_file(char * , seller_Details );
-
-
 void design() ;
-
 seller_Details *seller(seller_Details * , product_details *);
-
 seller_Details *login_seller(seller_Details *,char * , char *);
-
 void check_the_buyer_request(seller_Details *);
-
 seller_Details * login(seller_Details *);
 int new_registration_to_file();
 
+//functions for product_details
+
 product_details *product_registration(product_details *);
-
 product_details data_extraction_from_buffer(char * , product_details , int *);
-
 product_details *create_list_for_product(product_details * , product_details);
-
 product_details *delete_product(product_details * ,int);
-
 product_details * modify_product_details(product_details * ,int,seller_Details *);
 void display_product(product_details *);
-
 void adding_produt_into_file();
-
 void save_product_info_file(product_details *);
 void adding_the_product_into_file(int);
-
-int alpha_validator(char *);
-int password_validation(char *);
-
 void history_display();
-
 seller_Details *get_the_seller_dwtails_for_product(product_details * ,int , seller_Details *);
 
-
-
-bidding_details *biding_histroy_extraction_from_file(bidding_details *);
-
-
-int finding_the_largest_bidding_price(bidding_details);
-
-bidding_details *create_bidding_list(bidding_details * , bidding_details);
-
-bidding_details *check_for_best_price_by_buyer(bidding_details *  , bidding_details );
-
-void display_bidding_price(bidding_details *);
-
-void saving_tranction_into_file(bidding_details *);
-
+//functions for validation
+int alpha_validator(char *);
+int password_validation(char *);
 int int_ans_choice(int , int);
 
 int intiger_validation(char *);
@@ -106,3 +85,12 @@ int int_id_validation();
 int Emai_validation(char *);
 int validate_Pan(char *);
 
+
+//functions for bidding
+
+bidding_details *biding_histroy_extraction_from_file(bidding_details *);
+int finding_the_largest_bidding_price(bidding_details);
+bidding_details *create_bidding_list(bidding_details * , bidding_details);
+bidding_details *check_for_best_price_by_buyer(bidding_details *  , bidding_details );
+void display_bidding_price(bidding_details *);
+void saving_tranction_into_file(bidding_details *);
