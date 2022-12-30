3e0c2fe26c1025424b0f9eca9dfe3a6784ab4602
Jayasree1802 <121030796+Jayasree1802@users.noreply.github.com>
2022-12-30 21:34:18 +0530

    Update header.h

--- CUT/CODE/HEADER/header.h
+++ CUT/CODE/HEADER/header.h
@@ -126,6 +126,24 @@ void adding_the_product_into_file(int);
 void history_display();
 seller_Details *get_the_seller_dwtails_for_product(product_details * ,int , seller_Details *);
 
+//functions for buyer
+
+buyer_Details *buyer(buyer_Details * , seller_Details *,product_details *);
+buyer_Details *registration_buyer(buyer_Details *);
+buyer_Details *buyer_login(buyer_Details *,char * , char *);
+
+void writing_buyer_info_to_file();
+
+buyer_Details data_extraction_from_buyer_file(char * ,buyer_Details);
+buyer_Details *create_list_buyer(buyer_Details * , buyer_Details);
+void display(seller_Details *);
+
+void auction_for_product(product_details * , seller_Details * , buyer_Details *);
+
+product_details *get_the_product_detail(product_details * ,int);
+
+void display_buyer(buyer_Details *);
+
 //functions for validation
 int alpha_validator(char *);
 int password_validation(char *);
